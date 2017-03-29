2e8839832f68e2fc3f15caba4f6dc938937185c6
Tsamis Vasilis <tsamis.vas@gmail.com>
2017-03-30 00:47:53 +0300

    1. Logging configuration and a log example in HomePageController
    2. MVC HomePageController was created and Freemarker example
    
    https://hellokoding.com/spring-boot-hello-world-example-with-freemarker/

---
+++ Trading-parent/Trading-web/src/main/java/gr/Accenture2/TradingPlatform/web/controller/HomePageController.java
@@ -0,0 +1,32 @@
+package gr.Accenture2.TradingPlatform.web.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+
+@EnableAutoConfiguration
+public class HomePageController {
+
+	/** The logger in use by this controller. */
+	private static final Logger LOGGER = LoggerFactory.getLogger(HomePageController.class);
+
+	
+	//@RequestMapping(value = "/{pageName}", method = RequestMethod.GET)
+	//@ResponseBody
+	
+	@RequestMapping("/")
+	public String showPage( Model model) {
+		
+		LOGGER.debug("This is a test debug log");
+		
+        return "home";
+    }
+	
+}
