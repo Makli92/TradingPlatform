0bb8b000ac7c7d830a55624d6b851304fd732567
Makli Tahiri <kostahiri@gmail.com>
2017-04-08 14:20:38 -0800

    Config built path.

---
+++ Trading-parent/Trading-persistence/src/main/java/gr/Accenture2/TradingPlatform/models/Order.java
@@ -0,0 +1,32 @@
+package gr.Accenture2.TradingPlatform.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table
+public class Order {
+	@Id
+	@GeneratedValue( strategy = GenerationType.AUTO )
+	private int id;
+
+	public Order() {
+		super();
+	}
+
+	public Order(int id) {
+		super();
+		this.id = id;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+}
