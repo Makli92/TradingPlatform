d97991d14276d1141e5e7c1244d6be360a15af70
Tsamis Vasilis <tsamis.vas@gmail.com>
2017-04-26 21:28:32 +0300

    Create data url

--- Trading-parent/Trading-web/src/main/java/gr/Accenture2/TradingPlatform/service/RoleServiceImpl.java
+++ Trading-parent/Trading-web/src/main/java/gr/Accenture2/TradingPlatform/service/RoleServiceImpl.java
@@ -28,5 +28,10 @@ public class RoleServiceImpl implements RoleService {
 		
 	}
 	
+	public Role save(Role role){
+		
+		return roleRepository.save(role);
+	}
+	
 	
 }
--- Trading-parent/Trading-web/src/main/java/gr/Accenture2/TradingPlatform/web/controller/HomePageController.java
+++ Trading-parent/Trading-web/src/main/java/gr/Accenture2/TradingPlatform/web/controller/HomePageController.java
@@ -1,13 +1,18 @@
 package gr.Accenture2.TradingPlatform.web.controller;
 
+import java.util.HashSet;
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import gr.Accenture2.TradingPlatform.core.entity.Company;
+import gr.Accenture2.TradingPlatform.core.entity.Role;
+import gr.Accenture2.TradingPlatform.core.entity.User;
+import gr.Accenture2.TradingPlatform.core.enumeration.StringEnumeration;
 import gr.Accenture2.TradingPlatform.repository.service.CompanyRepository;
 import gr.Accenture2.TradingPlatform.service.CompanyService;
+import gr.Accenture2.TradingPlatform.service.RoleService;
 import gr.Accenture2.TradingPlatform.service.StockService;
 import gr.Accenture2.TradingPlatform.service.TradeService;
 import gr.Accenture2.TradingPlatform.service.UserService;
@@ -51,6 +56,9 @@ public class HomePageController {
 	@Autowired 
 	UserService userService;
 	
+	@Autowired 
+	RoleService roleService;
+	
 	//@RequestMapping(value = "/{pageName}", method = RequestMethod.GET)
 	//@ResponseBody
 	
@@ -87,49 +95,49 @@ public class HomePageController {
 	}
 
 	
-	@RequestMapping("/create")
-	public ModelAndView create() throws Exception {
+	@RequestMapping("/createData")
+	public ModelAndView createData() throws Exception {
 		
-		companyService.createCompany("Accenture", 10L, 10);
-			
-		//LOGGER.debug("findUnpurchasedStocks:{}", stockService.findUnpurchasedStocks(2));
+		Role role = new Role();
+		role.setRole("Admin");
+		roleService.save(role);
 		
-		//LOGGER.debug("findPurchasedStocks:{}",stockService.findPurchasedStocks(companyService.findByName("Accenture"), 1));
+		role = new Role();
+		role.setRole("User");
+		roleService.save(role);
+		
+		
+		
+		User user1  =  new User();
+		user1.setUsername("Bill");
+		user1.setEnabled(true);
+		user1.setCashBalance(100);
+		user1.setPassword("321");
+		user1.setEmail("test@test.com");
+		user1.setRoles(new HashSet<Role>());
+		user1.getRoles().add(roleService.findByRole(StringEnumeration.USER.getString()));
+		userService.save(user1);
+		
+		companyService.createCompany("Accenture", 10L, 30);
 	
-		//tradeService.purchaseStocks(companyService.findByName("Accenture"), 2, userService.findByUsername("Bill"));
+		tradeService.purchaseStocks(companyService.findByName("Accenture"), 3, userService.findByUsername("Bill"));
 		
-		ModelAndView mnv = new ModelAndView("index");
-	    return mnv;
-	}
-	
-	@RequestMapping("/buy")
-	public ModelAndView save() throws Exception {
-		
-		//companyService.createCompany("Accenture", 10L, 10);
-			
-		//LOGGER.debug("findUnpurchasedStocks:{}", stockService.findUnpurchasedStocks(2));
-		
-		//LOGGER.debug("findPurchasedStocks:{}",stockService.findPurchasedStocks(companyService.findByName("Accenture"), 1));
-	
-		tradeService.purchaseStocks(companyService.findByName("Accenture"), 2, userService.findByUsername("Bill"));
-		
-		ModelAndView mnv = new ModelAndView("index");
-	    return mnv;
-	}
-	
-	@RequestMapping("/sell")
-	public ModelAndView get() throws Exception {
-		
-		//companyService.createCompany("Accenture", 10L, 10);
-			
-		//LOGGER.debug("findUnpurchasedStocks:{}", stockService.findUnpurchasedStocks(2));
 		
 		tradeService.sellStocks(companyService.findByName("Accenture"),1,userService.findByUsername("Bill"));
 		
-		//LOGGER.debug("findPurchasedStocks:{}",stockService.findPurchasedStocks(companyService.findByName("Accenture"), 1));
-
+		
 		ModelAndView mnv = new ModelAndView("index");
 	    return mnv;
 	}
+	
+	@RequestMapping("/test")
+	public ModelAndView create() throws Exception {
+
+		//LOGGER.debug("findUnpurchasedStocks:{}", stockService.findUnpurchasedStocks(2));
+		
+		ModelAndView mnv = new ModelAndView("index");
+	    return mnv;
+	}
+	
 	
 }
